60e81a1c9bc73d57d85f38f23120afdc0a9bd3ee
mingyuchoo <mingyuchoo@gmail.com>
2025-04-10 21:41:48 +0900

    added

--- sbcl/sbcl-web-service/start-server.lisp
+++ sbcl/sbcl-web-service/start-server.lisp
@@ -1,14 +1,6 @@
-#!/bin/bash
-
-# Create a startup script for SBCL
-cat > start-server.lisp << 'EOF'
 (load "start.lisp")
 (format t "~%Starting server in the background...~%")
 (sb-thread:make-thread #'sbcl-web-service:main)
 (format t "Server should now be running. Test with: curl http://localhost:8080/api/health~%")
 (format t "Press Ctrl+C to stop the server~%")
 (loop (sleep 3600))
-EOF
-
-# Run SBCL with the startup script
-sbcl --load start-server.lisp
